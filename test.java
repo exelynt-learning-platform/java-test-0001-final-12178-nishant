7e7310ea1361552916b737b700617a67b7529cfc
ernbwayfal1943-cell <er.nbwayfal1943@gmail.com>
2026-03-14 10:46:48 +0530

    Update test.java

--- test.java
+++ test.java
@@ -1,16 +1,48 @@
-public class Pattern {
+public class DiamondPattern {
+
     public static void main(String[] args) {
 
-        int rows = 6;
+        int n = 5;
 
-        for (int i = 1; i <= rows; i++) {
+        // Upper part
+        for (int i = 1; i <= n; i++) {
 
-            for (int j = 1; j <= 2; j++) {
+            // Print leading spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
 
-                if (i == 1 || j == 1 || j == 2) {
-                    System.out.print("* ");
+            // Print first star
+            System.out.print("*");
+
+            // Print inner spaces and second star
+            if (i > 1) {
+                for (int j = 1; j <= (2 * i - 3); j++) {
+                    System.out.print(" ");
                 }
+                System.out.print("*");
+            }
 
+            System.out.println();
+        }
+
+        // Lower part
+        for (int i = n - 1; i >= 1; i--) {
+
+            // Print leading spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+
+            // Print first star
+            System.out.print("*");
+
+            // Print inner spaces and second star
+            if (i > 1) {
+                for (int j = 1; j <= (2 * i - 3); j++) {
+                    System.out.print(" ");
+                }
+                System.out.print("*");
             }
 
             System.out.println();
